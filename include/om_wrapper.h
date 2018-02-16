cefe12f3490d8ba10b32763daf2d73215fe2bcca
Vincent Tavernier <vince.tavernier@gmail.com>
2018-02-16 11:34:13 +0100

    Added documentation to wrapper

--- include/om_wrapper.h
+++ include/om_wrapper.h
@@ -4,6 +4,9 @@
 #include <string>
 #include <functional>
 
+/**
+ * Represents the type of wrapper objects.
+ */
 enum OMWrapperType {
 #if OMW_MATHEMATICA
     OMWT_MATHEMATICA,
@@ -13,6 +16,9 @@ enum OMWrapperType {
 #endif
 };
 
+/**
+ * Template definition for wrapper objects
+ */
 template<OMWrapperType WType>
 class OMWrapper {
 };
@@ -21,23 +27,58 @@ class OMWrapper {
 
 #include "mathlink.h"
 
+/**
+ * Represents the interface wrapper for Mathematica (MathLink) code.
+ */
 template<>
 class OMWrapper<OMWT_MATHEMATICA> {
+    /// Id of the next parameter to be retrieved
     size_t currentParamIdx;
+    /// Reference to the link object to use
     MLINK &link;
+    /// Name of the namespace where symbols and messages are defined
     std::string mathNamespace;
 
 public:
+    /**
+     * Constructs a new Mathematica interface wrapper
+     * @param mathNamespace Name of the namespace where symbols and messages are defined
+     * @param link          Link object to use to communicate with the Kernel
+     */
     OMWrapper(const std::string &mathNamespace, MLINK &link);
 
+    /**
+     * Obtains the value of a given parameter.
+     * @param  paramIdx  Ordinal index of the parameter, for consistency with the
+     *                   calling pattern in the tm file.
+     * @param  paramName User-friendly name of the parameter, for debugging purposes.
+     * @return           The value of the parameter.
+     * @throws std::runtime_error When the current parameter index does not match
+     * the ordinal index of this parameter, or the value is not of the expected type.
+     */
     template<typename T>
     T GetParam(size_t paramIdx, const std::string &paramName);
 
+    /**
+     * Runs a function using the state of the link associated with this interface
+     * wrapper.
+     * @param fun Function to invoke when the link is ready.
+     */
     void RunFunction(std::function<void(void)> &&fun);
 
 private:
+    /**
+     * Ensures the current parameter matches the parameter requested by the caller.
+     * @param paramIdx  Ordinal index of the parameter
+     * @param paramName User-friendly name of the parameter
+     * @throws std::runtime_error See GetParam for details.
+     */
     void CheckParameterIdx(size_t paramIdx, const std::string &paramName);
 
+    /**
+     * Sends a failure message on the link object to notify of a failure.
+     * @param exceptionMessage Text to send in the message.
+     */
     void SendFailure(const char *exceptionMessage);
 };
 
@@ -47,6 +88,6 @@ bool OMWrapper<OMWT_MATHEMATICA>::GetParam<bool>(size_t paramIdx, const std::str
 template<>
 int OMWrapper<OMWT_MATHEMATICA>::GetParam<int>(size_t paramIdx, const std::string &paramName);
 
-#endif
+#endif /* OMW_MATHEMATICA */
 
 #endif /* _OM_WRAPPER_H_ */
