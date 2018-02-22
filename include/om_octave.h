0306395d9f3a0b1127c214d4f4a753f20fa59952
Vincent Tavernier <vince.tavernier@gmail.com>
2018-02-22 18:44:52 +0100

    Split project sources and add basic CMakeLists.txt for add_subdirectory

---
+++ include/om_octave.h
@@ -0,0 +1,163 @@
+#ifndef _OM_OCTAVE_H_
+#define _OM_OCTAVE_H_
+
+#if OMW_OCTAVE
+
+#include <oct.h>
+#include <parse.h>
+
+/**
+ * Represents the interface wrapper for Mathematica (MathLink) code.
+ */
+class OMWrapperOctave : public OMWrapperBase
+{
+	/// Current list of arguments
+	const octave_value_list *currentArgs;
+	/// Current list of values to return
+	octave_value_list result;
+
+	public:
+
+	/**
+	 * Constructs a new Octave interface wrapper
+	 * @param userInitializer User initialization function.
+	 */
+	OMWrapperOctave(std::function<void(void)> userInitializer = std::function<void(void)>());
+
+	inline octave_value_list &Result() { return result; }
+
+	inline const octave_value_list &Args() { return *currentArgs; }
+
+	/**
+	 * Obtains the value of a given parameter.
+	 * @param  paramIdx  Ordinal index of the parameter, for consistency with the
+	 *                   calling pattern in the tm file.
+	 * @param  paramName User-friendly name of the parameter, for debugging purposes.
+	 * @return           The value of the parameter.
+	 * @throws std::runtime_error When the current parameter index does not match
+	 * the ordinal index of this parameter, or the value is not of the expected type.
+	 */
+	template <class... Types, typename Indices = void, typename = typename std::enable_if<(sizeof...(Types) == 1)>::type>
+	typename std::tuple_element<0, std::tuple<Types...>>::type GetParam(size_t paramIdx, const std::string &paramName);
+
+	/**
+	 * Obtains the value of a given parameter. If the value returned on the link
+	 * is a Null symbol, then this parameter is considered to have no value,
+	 * and an empty boost::optional is returned.
+	 * @param  paramIdx  Ordinal index of the parameter.
+	 * @param  paramName User-friendly name of the parameter.
+	 * @return           The value of the parameter, or an empty optional.
+	 * @throws std::runtime_error
+	 */
+	template <typename T>
+	boost::optional<T> GetOptionalParam(size_t paramIdx, const std::string &paramName)
+	{
+		if (paramIdx >= currentArgs->length())
+		{
+			return boost::optional<T>();
+		}
+
+		return boost::optional<T>(GetParam<T>(paramIdx, paramName));
+	}
+
+	template <class... Types, typename Indices = std::make_index_sequence<sizeof...(Types)>,
+		typename = typename std::enable_if<(sizeof...(Types) > 1)>::type>
+	std::tuple<Types...> GetParam(size_t firstParamIdx, const std::string &paramName)
+	{
+		// Check first parameter location
+		CheckParameterIdx(firstParamIdx, paramName);
+
+		// Check enough args are available
+		if (firstParamIdx + sizeof...(Types) >= currentArgs->length())
+		{
+			std::stringstream ss;
+			ss << "Not enough args for building a tuple of size "
+			   << sizeof...(Types) << " for parameter " << paramName
+			   << " at index " << firstParamIdx;
+			throw std::runtime_error(ss.str());
+		}
+
+		std::tuple<Types...> result(GetTupleParamImpl<Types...>(firstParamIdx, paramName, Indices{}));
+
+		return result;
+	}
+
+
+	/**
+	 * Runs a function using the state of the link associated with this interface
+	 * wrapper.
+	 * @param args Octave arguments to the function
+	 * @param fun Function to invoke when the link is ready.
+	 * @return Octave list of return values
+	 */
+	octave_value_list RunFunction(const octave_value_list &args, std::function<void(OMWrapperOctave &)> fun);
+
+	/**
+	 * Evaluates the given function, assuming its execution returns a result
+	 * @param fun Code to execute to return the result
+	 */
+	void EvaluateResult(std::function<void(void)> fun)
+	{
+		fun();
+	}
+
+	/**
+	 * Sends a failure message on the link object to notify of a failure.
+	 * @param exceptionMessage Text to send in the message
+	 * @param messageName      Name of the format string to use
+	 */
+	void SendFailure(const std::string &exceptionMessage, const std::string &messageName = std::string("err"));
+
+	private:
+	/**
+	 * Ensures the current parameter matches the parameter requested by the caller.
+	 * @param paramIdx  Ordinal index of the parameter
+	 * @param paramName User-friendly name of the parameter
+	 * @throws std::runtime_error See GetParam for details.
+	 */
+	void CheckParameterIdx(size_t paramIdx, const std::string &paramName);
+
+	/**
+	 * Implementation of GetTupleParam variadic template function
+	 */
+	template <class... Types, std::size_t... I>
+	decltype(auto) GetTupleParamImpl(size_t paramIdx, const std::string &paramName, std::index_sequence<I...>)
+	{
+		return std::tuple<Types...>{GetParam<Types>(paramIdx + I, paramName)...};
+	}
+};
+
+template <>
+bool OMWrapperOctave::GetParam<bool>(size_t paramIdx, const std::string &paramName);
+
+template <>
+int OMWrapperOctave::GetParam<int>(size_t paramIdx, const std::string &paramName);
+
+template <>
+float OMWrapperOctave::GetParam<float>(size_t paramIdx, const std::string &paramName);
+
+template <>
+std::string OMWrapperOctave::GetParam<std::string>(size_t paramIdx, const std::string &paramName);
+
+template <>
+std::shared_ptr<OMArray<float>>
+OMWrapperOctave::GetParam<std::shared_ptr<OMArray<float>>>(size_t paramIdx,
+																	   const std::string &paramName);
+
+template <>
+std::shared_ptr<OMMatrix<float>>
+OMWrapperOctave::GetParam<std::shared_ptr<OMMatrix<float>>>(size_t paramIdx,
+																		const std::string &paramName);
+
+
+#define OM_RESULT_OCTAVE(w,code) w.EvaluateResult(code)
+#define OM_OCTAVE(w,code) w.ConditionalRun(code)
+
+#else /* OMW_OCTAVE */
+
+#define OM_RESULT_OCTAVE(w,code)
+#define OM_OCTAVE(w,code)
+
+#endif /* OMW_OCTAVE */
+
+#endif /* _OM_OCTAVE_H_ */
---
+++ src/om_mathematica.cpp
@@ -0,0 +1,268 @@
+#include <cstring>
+#include <sstream>
+
+#include "om_wrapper_base.h"
+#include "om_mathematica.h"
+
+#if OMW_MATHEMATICA
+
+OMWrapperMathematica::OMWrapperMathematica(const std::string &mathNamespace, MLINK &link,
+									   std::function<void(void)> userInitializer)
+: OMWrapperBase(std::forward<std::function<void(void)>>(userInitializer)),
+  currentParamIdx(std::numeric_limits<size_t>::max()), link(link), mathNamespace(mathNamespace)
+{
+}
+
+void OMWrapperMathematica::CheckParameterIdx(size_t paramIdx, const std::string &paramName)
+{
+	if (currentParamIdx != paramIdx)
+	{
+		std::stringstream ss;
+		ss << "Requested parameter " << paramName << " at index " << paramIdx
+		   << " while the current available parameter is at index " << currentParamIdx;
+		throw std::runtime_error(ss.str());
+	}
+}
+
+bool OMWrapperMathematica::RunFunction(std::function<void(OMWrapperMathematica &)> fun)
+{
+	try
+	{
+		currentParamIdx = 0;
+		hasResult = false;
+
+		fun(*this);
+
+		if (!hasResult)
+		{
+			MLPutSymbol(link, "Null");
+		}
+	}
+	catch (std::runtime_error &ex)
+	{
+		SendFailure(ex.what());
+	}
+
+	currentParamIdx = std::numeric_limits<size_t>::max();
+	return true;
+}
+
+void OMWrapperMathematica::SendFailure(const std::string &exceptionMessage, const std::string &messageName)
+{
+	MLPutFunction(link, "CompoundExpression", 2);
+	MLPutFunction(link, "Message", 2);
+	MLPutFunction(link, "MessageName", 2);
+	MLPutSymbol(link, mathNamespace.c_str());
+	MLPutString(link, messageName.c_str());
+	MLPutString(link, exceptionMessage.c_str());
+	MLPutSymbol(link, "$Failed");
+
+	// Note that SendFailure sends a result
+	hasResult = true;
+}
+
+template <>
+bool OMWrapperMathematica::GetParam<bool>(size_t paramIdx, const std::string &paramName)
+{
+	CheckParameterIdx(paramIdx, paramName);
+
+	const char *paramSymbol;
+	bool paramValue;
+
+	if (!MLGetSymbol(link, &paramSymbol))
+	{
+		MLClearError(link);
+
+		std::stringstream ss;
+		ss << "Failed to read symbol for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	// will delete afterwards
+	std::shared_ptr<const char> pSymbol(paramSymbol,
+										[this](const char *p) { MLReleaseSymbol(link, p); });
+
+	currentParamIdx++;
+
+	if (std::strcmp(paramSymbol, "True") == 0)
+	{
+		paramValue = true;
+	}
+	else if (std::strcmp(paramSymbol, "False") == 0)
+	{
+		paramValue = false;
+	}
+	else
+	{
+		std::stringstream ss;
+		ss << "Unknown symbol " << paramSymbol << " for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	return paramValue;
+}
+
+template <>
+int OMWrapperMathematica::GetParam<int>(size_t paramIdx, const std::string &paramName)
+{
+	CheckParameterIdx(paramIdx, paramName);
+
+	int paramValue;
+	if (!MLGetInteger32(link, &paramValue))
+	{
+		MLClearError(link);
+
+		std::stringstream ss;
+		ss << "Failed to get integer for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	currentParamIdx++;
+
+	return paramValue;
+}
+
+template <>
+float OMWrapperMathematica::GetParam<float>(size_t paramIdx, const std::string &paramName)
+{
+	CheckParameterIdx(paramIdx, paramName);
+
+	float paramValue;
+	if (!MLGetReal32(link, &paramValue))
+	{
+		MLClearError(link);
+
+		std::stringstream ss;
+		ss << "Failed to get float for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	currentParamIdx++;
+
+	return paramValue;
+}
+
+template <>
+std::string OMWrapperMathematica::GetParam<std::string>(size_t paramIdx, const std::string &paramName)
+{
+	CheckParameterIdx(paramIdx, paramName);
+
+	const char *paramValue;
+	if (!MLGetString(link, &paramValue))
+	{
+		MLClearError(link);
+
+		std::stringstream ss;
+		ss << "Failed to get string for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	currentParamIdx++;
+
+	std::string paramResult(paramValue);
+	MLReleaseString(link, paramValue);
+
+	return paramResult;
+}
+
+template <>
+std::shared_ptr<OMArray<float>>
+OMWrapperMathematica::GetParam<std::shared_ptr<OMArray<float>>>(size_t paramIdx, const std::string &paramName)
+{
+	CheckParameterIdx(paramIdx, paramName);
+
+	// Get the array
+	float *arrayData;
+	int arrayLen;
+
+	if (!MLGetReal32List(link, &arrayData, &arrayLen))
+	{
+		MLClearError(link);
+
+		std::stringstream ss;
+		ss << "Failed to get Real32List for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	currentParamIdx++;
+
+	// Delete array when out of scope
+	std::shared_ptr<OMArray<float>> arrayPtr(new OMArray<float>(arrayData, arrayLen),
+											 [this](OMArray<float> *p) {
+												 MLReleaseReal32List(link, const_cast<float*>(p->data()), p->size());
+												 delete p;
+											 });
+
+	return arrayPtr;
+}
+
+template <>
+std::shared_ptr<OMMatrix<float>>
+OMWrapperMathematica::GetParam<std::shared_ptr<OMMatrix<float>>>(size_t paramIdx, const std::string &paramName)
+{
+	CheckParameterIdx(paramIdx, paramName);
+
+	// Get the array
+	float *arrayData;
+	int *arrayDims;
+	int arrayDepth;
+	char **arrayHeads;
+
+	if (!MLGetReal32Array(link, &arrayData, &arrayDims, &arrayHeads, &arrayDepth))
+	{
+		MLClearError(link);
+
+		std::stringstream ss;
+		ss << "Failed to get Real32Array for parameter " << paramName << " at index " << paramIdx;
+		throw std::runtime_error(ss.str());
+	}
+
+	currentParamIdx++;
+
+	// Delete array when out of scope
+	std::shared_ptr<OMMatrix<float>> matrixPtr(new OMMatrix<float>(arrayData, arrayDims, arrayDepth, arrayHeads),
+											   [this](OMMatrix<float> *p) {
+												   MLReleaseReal32Array(link, const_cast<float*>(p->data()), p->dims(),
+																		p->heads(), p->depth());
+												   delete p;
+											   });
+
+	return matrixPtr;
+}
+
+#if OMW_INCLUDE_MAIN
+
+int omw_main(int argc, char *argv[]) { return MLMain(argc, argv); }
+
+// Entry point for MathLink
+
+#if WINDOWS_MATHLINK
+
+#if __BORLANDC__
+#pragma argsused
+#endif
+
+int PASCAL WinMain(HINSTANCE hinstCurrent, HINSTANCE hinstPrevious, LPSTR lpszCmdLine, int nCmdShow)
+{
+	char buff[512];
+	char FAR *buff_start = buff;
+	char FAR *argv[32];
+	char FAR *FAR *argv_end = argv + 32;
+
+	hinstPrevious = hinstPrevious; /* suppress warning */
+
+	if (!MLInitializeIcon(hinstCurrent, nCmdShow))
+		return 1;
+	MLScanString(argv, &argv_end, &lpszCmdLine, &buff_start);
+	return omw_main((int)(argv_end - argv), argv);
+}
+
+#else /* WINDOWS_MATHLINK */
+
+int main(int argc, char *argv[]) { return omw_main(argc, argv); }
+
+#endif /* WINDOWS_MATHLINK */
+
+#endif /* OMW_INCLUDE_MAIN */
+
+#endif /* OMW_MATHEMATICA */
